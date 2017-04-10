4905a0d486cb77983531d02c8ceaa5fe32622a11
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2017-04-10 15:59:33 +0900

    Made changes to separate out noiseFP and nonNoiseFP. Added a function to calculate metrics from errors

--- SegmentationQualityMetrics.py
+++ SegmentationQualityMetrics.py
@@ -34,8 +34,21 @@ def labelCentroidRadius(labelImage: sitk.Image) -> tuple:
     return labels, centroids, radii
 
 
-def segQualMetrics(testLabelImageFile: str,
-                   groundTruthLabeImageFile: str) -> tuple:
+def getMetricsFromCounts(nFP: float, nTP: float, nFN: float) -> typing.List[float]:
+
+    recall = nTP / (nTP + nFN)
+
+    precision = nTP / (nTP + nFP)
+
+    fMeasure = 2 * (recall * precision) / (recall + precision)
+
+    accuracy = nTP / (nTP + nFN + nFP)
+
+    return recall, precision, fMeasure, accuracy
+
+
+def segQualErrors(testLabelImageFile: str,
+                  groundTruthLabeImageFile: str) -> tuple:
 
     testLabelImage = sitk.ReadImage(testLabelImageFile)
     gtLabelImage = sitk.ReadImage(groundTruthLabeImageFile)
@@ -57,30 +70,44 @@ def segQualMetrics(testLabelImageFile: str,
 
     truePositiveMask = np.less_equal(nnDists, gtRadii)
     testTPInds = nnInds[truePositiveMask]
-    testTPs = [testLabels[x] for x in testTPInds]
+    testTPInds = list(set(testTPInds))
+    testTPLabels = [testLabels[x] for x in testTPInds]
 
-    nTruePostive = len(testTPs)
+    testFPLabels = [x for x in testLabels if x not in testTPLabels]
+    testFPCentroids = [y for x, y in zip(testLabels, testCentroids) if x not in testTPLabels]
+
+    if len(testFPLabels):
+
+        gtCentroidKDTree = cKDTree(gtCentroids, leafsize=100)
+        # looking for nearest neighbours for among gtCentroids for each testFP
+        gtNNDists, gtNNInds = gtCentroidKDTree.query(testFPCentroids)
+        # looking those FPs whose centroids are not in any gt sphere
+        noiseFPIndsAmongTestFPs = [x for x, y in enumerate(gtNNDists) if y > gtRadii[x]]
+        noiseFPLabels = [testFPLabels[x] for x in noiseFPIndsAmongTestFPs]
+
+        nonNoiseFPLabels = [x for x in testFPLabels if x not in noiseFPLabels]
+
+    else:
+        noiseFPLabels = []
+        nonNoiseFPLabels = []
+
+    nTP = len(testTPLabels)
 
     nGT = len(gtLabels)
 
     nTest = len(testLabels)
 
-    nFalsePositive = nTest - nTruePostive
+    nFP = nTest - nTP
 
-    nFalseNegative = nGT - nTruePostive
+    nFN = nGT - nTP
 
-    recall = nTruePostive / nGT
+    nNoiseFP = len(noiseFPLabels)
+    nNonNoiseFP = len(nonNoiseFPLabels)
 
-    precision = nTruePostive / nTest
+    assert nNoiseFP + nNonNoiseFP == nFP, "NoiseFPs and NonNoiseFPs don't union up to FalsePositives"
 
-    fMeasure = 2 * (recall * precision) / (recall + precision)
+    return nFP, nTP, nFN, nNoiseFP, nNonNoiseFP
 
-    accuracy = nTruePostive / (nTruePostive + nFalseNegative + nFalsePositive)
-
-    testCellCounts = nTest
-    gtCellCount = nGT
-
-    return gtCellCount, testCellCounts, recall, precision, fMeasure, accuracy
 
 def saveResultsTestList(testLabelImageFiles: typing.Iterable[str],
                         groundTruthLabelImagFile: str, outputDir: str,
@@ -92,13 +119,16 @@ def saveResultsTestList(testLabelImageFiles: typing.Iterable[str],
     resDF = pd.DataFrame()
 
     for label, testLabelImageFile in zip(labels, testLabelImageFiles):
-
-        gtCellCount, testCellCounts, recall, precision, fMeasure, accuracy = \
-            segQualMetrics(testLabelImageFile, groundTruthLabelImagFile)
+        nFP, nTP, nFN, nNoiseFP, nNonNoiseFP= \
+            segQualErrors(testLabelImageFile, groundTruthLabelImagFile)
+        recall, precision, fMeasure, accuracy = getMetricsFromCounts(nFP, nTP, nFN)
+        testCellCount = nFP + nTP
+        gtCellCount = nTP + nFN
         tempDict = {'Recall': recall, 'Precision': precision, 'fMeasure': fMeasure, 'Accuracy': accuracy,
                     'testLabelImageFile': testLabelImageFile,
                     'groundTruthLabelImageFile': groundTruthLabelImagFile,
-                    'label': label, 'testCellCount': testCellCounts, 'groundTruthCellCount': gtCellCount}
+                    'label': label, 'testCellCount': testCellCount, 'groundTruthCellCount': gtCellCount,
+                    'nFP': nFP, 'nTP': nTP, 'nFN': nFN, 'nNoiseFP': nNoiseFP, 'nNonNoiseFP': nNonNoiseFP}
         resDF = resDF.append(tempDict, ignore_index=True)
 
     fig0, ax0 = plt.subplots(figsize=(14, 11.2))
@@ -139,8 +169,13 @@ if __name__ == '__main__':
     testLabelImageFile = sys.argv[1]
     gTLabelImageFile = sys.argv[2]
 
-    recall, precision, fMeasure, accuracy = segQualMetrics(testLabelImageFile=testLabelImageFile,
-                                                           groundTruthLabeImageFile=gTLabelImageFile)
+    nFP, nTP, nFN, nNoiseFP, nNonNoiseFP = segQualErrors(testLabelImageFile=testLabelImageFile,
+                                                         groundTruthLabeImageFile=gTLabelImageFile)
+    nTest = nFP + nTP
+    nGT = nTP + nFN
+
+    recall, precision, fMeasure, accuracy = getMetricsFromCounts(nFP, nTP, nFN)
+    print('nFP={}\nnTP={}\nnFN={}\nnNoiseFP={}\nnNonNoiseFP={}'.format(nFP, nTP, nFN, nNoiseFP, nNonNoiseFP))
     print('Recall={}\nPrecision={}\nfMeasure={}\nAccuracy={}'.format(recall, precision, fMeasure, accuracy))
 
 
